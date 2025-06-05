81323d0b0c6ab1320d27fba0706133e4bcd09ec1
siliproksi <162310474+siliproksi@users.noreply.github.com>
2025-06-05 05:29:09 +0100

    Update Recommendations.jsx with correct backend url

--- vite-project/src/components/Recommendations.jsx
+++ vite-project/src/components/Recommendations.jsx
@@ -8,7 +8,7 @@ function Recommendations() {
 
     function fetchProducts() {
         try {
-            fetch('http://localhost:3000/api/products', { signal: AbortSignal.timeout(2000) })
+            fetch('https://3624-216-158-90-135.ngrok-free.app/api/products', { signal: AbortSignal.timeout(2000) })
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
